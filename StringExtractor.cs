1bdcf5de18448ae84485003927773f22e1dcfb01
Nasoki-Koide <95671212+Nasoki-Koide@users.noreply.github.com>
2023-09-26 23:11:44 +0900

    feat: #1 Overloaded.

--- StringExtractor.cs
+++ StringExtractor.cs
@@ -6,7 +6,20 @@ namespace StringExtractors
     public static class StringExtractor
     {
         public static ExtractionResult Extract(
-            string source, LeftString leftString, RightString rightString, SearchOrder searchOrder)
+            string source, string leftString = null, string rightString = null)
+        {
+            ILeftString ls = string.IsNullOrEmpty(leftString) ?
+                (ILeftString)new NullLeftString() : new LeftString(leftString);
+
+            IRightString rs = string.IsNullOrEmpty(rightString) ?
+                (IRightString)new NullRightString() : new RightString(rightString);
+
+            return Extract(source, ls, rs);
+        }
+
+        public static ExtractionResult Extract(
+            string source, ILeftString leftString, IRightString rightString,
+            SearchOrder searchOrder = SearchOrder.LeftFirst)
         {
             var cutter = new StringCutter(source, searchOrder, leftString, rightString);
             return new ExtractionResult(cutter.Cut());
